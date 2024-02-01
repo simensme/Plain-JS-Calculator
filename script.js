f4f79d156fc8b1e029d13ab196c8362ae049d344
Simen Smehaugen <simdevgen@outlook.com>
2024-02-01 13:41:11 +0100

    added mathematical computation of number inputs

--- script.js
+++ script.js
@@ -28,19 +28,34 @@ const addDecimal = () => {
     }
 };
 
+// Calculate first and second values, according to the operator used
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber,
+};
+
 // Function to use different operators
 const useOperator = operator => {
     const currentValue = Number(calculatorDisplay.textContent);
+    // Prevent multiple operators
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    }
     // Assign firstValue if no value
     if (!firstValue) {
         firstValue = currentValue;
     } else {
-        console.log('currentvalue', currentValue);
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
     }
     // Ready for the next value, store operator
     awaitingNextValue = true;
     operatorValue = operator;
-    console.log('firstValue:', firstValue, 'operatorValue', operatorValue);
 };
 
 // Add Event Listeners for numbers, operators and decimal buttons
